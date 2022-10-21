10c1ee296969e4b039309b87c95ab3bcded4b42a
miralulu <mirai@katachi>
2022-10-21 11:12:55 +0900

    マイグレーション実行準備完了

---
+++ database/migrations/2022_10_20_062120_create_todo_items.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->bigIncrements('id')->nullable(false);
+            $table->date('expiration_date')->nullable(false);
+            $table->string('todo_items', 50)->nullable(false);
+            $table->tinyInteger('is_completed')->default(0)->nullable(false);
+            $table->tinyInteger('is_deleted')->default(0)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_items');
+    }
+}
