de8b58d2f9c59fc228f13df63bc1a060de2a9bc7
miralulu <mirai@katachi>
2022-10-26 15:09:22 +0900

    2022_1026

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use Illuminate\Foundation\Console\Presets\React;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\VIEW;
 use Validator;
 
 
@@ -18,7 +20,7 @@ class TodoController extends Controller
             ->where('is_deleted', 0)
             ->orderBy('expiration_date')
             ->orderBy('id')
-            ->get(); // 省略した場合はすべてのカラムを取得
+            ->get(); // 引数を省略した場合はすべてのカラムを取得
 
         $data = [
             'today' => date('Y-m-d'),
@@ -28,11 +30,22 @@ class TodoController extends Controller
         return view('todo.index', $data);
     }
 
-    public function edit(Request $request){
+    public function post(TodoRequest $request)
+    {
+        return view('todo.index');
+    }
+
+
+    public function edit(Request $request)
+    {
+        $items = DB::table('todo_items')
+            ->where('id', $request->id)
+            ->get();
+
         $data = [
-            $request->id
+            'items' => $items,
         ];
 
-        return view('todo.edit' $data);
+        return view('todo.edit', $data);
     }
 }
